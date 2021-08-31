299e9993c0b678d7c05304b04d281dbf683795ed
Dmitry-Mogilevich <89597468+Dmitry-Mogilevich@users.noreply.github.com>
2021-08-31 11:30:30 +0300

    Controller and router

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\BooksController;
+    use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', 'books');
+Route::resource('books', BooksController::class);
